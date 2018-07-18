18bc123ad129c4fe2b5d53e67e1cc5af3e75f26f
rrrainy <ryuuguu.rena1989@gmail.com>
2018-07-18 11:26:52 +0300

    init

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../../config');
 
-mongoose.connect(config.get('mongoose:uri'));
+mongoose.connect(config.get('mongoose:uri'),  {useNewUrlParser: true});
 
 module.exports = mongoose;
