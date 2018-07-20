11dc7c1de555ba38f0ba1fc865553cb7e763b0f0
rrrainy <ryuuguu.rena1989@gmail.com>
2018-07-20 11:15:54 +0300

    bugfix

--- src/telegram/trashCleaner.js
+++ src/telegram/trashCleaner.js
@@ -5,7 +5,7 @@ const { findChannels, timeout} = require('./methods')
 
 module.exports = async () => {
 
-    schedule.scheduleJob('* */10 * * *', async () => {
+    schedule.scheduleJob('* * 20 * *', async () => {
         const usernames = await Post.getUsernames();
 
         if(usernames.length === 0) return;
